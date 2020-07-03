4415a0196ac8559d10a5e5825f40ab0cf26020ce
Ole <ole@Oles-MBP-2.lyse.net>
2020-07-03 20:24:42 +0200

    Initial

---
+++ funkyfilter.ps1
@@ -0,0 +1,40 @@
+function Get-AllOrderedElementConfigurations {
+
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string[]]
+        $elements
+    )
+    $result = [System.Collections.Generic.list[string]]::new()
+
+    $elements = $elements | Select-Object -Unique | Sort-Object
+    $maxbinaryval = [System.Math]::Pow(2, ($elements | Measure-Object).Count) 
+
+    foreach ($binVal in 0..($maxbinaryval )) {
+        $tempResult = ""
+        $bitPos = [System.Collections.Generic.list[string]]::New()
+        $binaryString = [Convert]::ToString($binVal, 2)
+        $reversedBinaryStringArray = $binaryString.ToCharArray()
+        [array]::Reverse($reversedBinaryStringArray)
+        foreach ($bit in $reversedBinaryStringArray) {
+            $bitPos.add($bit)
+        }
+        for ($i = 0; $i -lt $bitPos.Count; $i++) {
+            if ($bitPos[$i] -eq "1") {
+                $tempResult += $elements[$i]
+            }
+        }
+        if (!$result.Contains($tempResult)) {
+            $result.Add($tempResult)
+        
+        }
+    }
+
+    $result | Sort-Object
+
+}
+
+
+
+
