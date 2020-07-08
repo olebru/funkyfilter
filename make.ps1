f690e03d42fc4f559287653e2c2babee5f183752
Ole <ole@Oles-MBP-2.lyse.net>
2020-07-08 12:57:53 +0200

    Beginning of module

---
+++ make.ps1
@@ -0,0 +1,6 @@
+$manifest = @{
+    Path              = '.\FunkyFilter\FunkyFilter.psd1'
+    RootModule        = 'FunkyFilter.psm1' 
+    Author            = 'Ole I Bruvik'
+}
+New-ModuleManifest @manifest
